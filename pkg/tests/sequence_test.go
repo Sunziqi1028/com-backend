18fa1f727e489e3c706c67b815f13b9fca8cadbb
Adennan <1257332619@qq.com>
2021-07-10 23:48:35 +0800

    fix snowflake algorithm

--- pkg/tests/sequence_test.go
+++ pkg/tests/sequence_test.go
@@ -1,1 +1,26 @@
 package pkg
+
+import (
+	"ceres/pkg/utility/net"
+	"ceres/pkg/utility/sequence"
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestSeq(t *testing.T) {
+	machineIP := net.GetDomianIP()
+	machineSignature := strings.Replace(machineIP, ".", "", 4)
+	machineID, err := strconv.ParseInt(machineSignature, 10, 64)
+	machineID %= 32
+	if err != nil {
+		return
+	}
+	flake := sequence.NewSnowflake(1525705533000, uint64(machineID))
+
+	for i := 0; i <= 4097; i++ {
+		fmt.Printf("%d\n", flake.Next())
+		fmt.Println(flake)
+	}
+}
