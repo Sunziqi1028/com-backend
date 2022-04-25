387209b59cd43f15e334b14fa8363164ba8b5c64
song mu <musong0058@sina.com>
2022-04-25 22:22:25 +0800

    startup eth listen update

--- pkg/event/abi.go
+++ pkg/event/abi.go
@@ -11,269 +11,210 @@ var (
 		Event:    "created",
 		EventHex: "0x73f46948d120a56e9fea7152c9545282fb4a4bb4d648c110ff98352525745595",
 		Abi: `[
-    {
-      "inputs": [],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "previousOwner",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "newOwner",
-          "type": "address"
-        }
-      ],
-      "name": "OwnershipTransferred",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": false,
-          "internalType": "string",
-          "name": "name",
-          "type": "string"
-        },
-        {
-          "components": [
-            {
-              "internalType": "string",
-              "name": "name",
-              "type": "string"
-            },
-            {
-              "internalType": "enum Startup.Mode",
-              "name": "mode",
-              "type": "uint8"
-            },
-            {
-              "internalType": "string[]",
-              "name": "hashtag",
-              "type": "string[]"
-            },
-            {
-              "internalType": "string",
-              "name": "logo",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "mission",
-              "type": "string"
-            },
-            {
-              "internalType": "address",
-              "name": "tokenContract",
-              "type": "address"
-            },
-            {
-              "components": [
-                {
-                  "internalType": "string",
-                  "name": "name",
-                  "type": "string"
-                },
-                {
-                  "internalType": "address",
-                  "name": "walletAddress",
-                  "type": "address"
-                }
-              ],
-              "internalType": "struct Startup.wallet[]",
-              "name": "wallets",
-              "type": "tuple[]"
-            },
-            {
-              "internalType": "string",
-              "name": "overview",
-              "type": "string"
-            },
-            {
-              "internalType": "bool",
-              "name": "isValidate",
-              "type": "bool"
-            }
-          ],
-          "indexed": false,
-          "internalType": "struct Startup.Profile",
-          "name": "startUp",
-          "type": "tuple"
-        },
-        {
-          "indexed": false,
-          "internalType": "address",
-          "name": "msg",
-          "type": "address"
-        }
-      ],
-      "name": "created",
-      "type": "event"
-    },
-    {
-      "stateMutability": "payable",
-      "type": "fallback"
-    },
-    {
-      "inputs": [
-        {
-          "components": [
-            {
-              "internalType": "string",
-              "name": "name",
-              "type": "string"
-            },
-            {
-              "internalType": "enum Startup.Mode",
-              "name": "mode",
-              "type": "uint8"
-            },
-            {
-              "internalType": "string[]",
-              "name": "hashtag",
-              "type": "string[]"
-            },
-            {
-              "internalType": "string",
-              "name": "logo",
-              "type": "string"
-            },
-            {
-              "internalType": "string",
-              "name": "mission",
-              "type": "string"
-            },
-            {
-              "internalType": "address",
-              "name": "tokenContract",
-              "type": "address"
-            },
-            {
-              "components": [
-                {
-                  "internalType": "string",
-                  "name": "name",
-                  "type": "string"
-                },
-                {
-                  "internalType": "address",
-                  "name": "walletAddress",
-                  "type": "address"
-                }
-              ],
-              "internalType": "struct Startup.wallet[]",
-              "name": "wallets",
-              "type": "tuple[]"
-            },
-            {
-              "internalType": "string",
-              "name": "overview",
-              "type": "string"
-            },
-            {
-              "internalType": "bool",
-              "name": "isValidate",
-              "type": "bool"
-            }
-          ],
-          "internalType": "struct Startup.Profile",
-          "name": "p",
-          "type": "tuple"
-        }
-      ],
-      "name": "newStartup",
-      "outputs": [],
-      "stateMutability": "payable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "name": "startups",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "name",
-          "type": "string"
-        },
-        {
-          "internalType": "enum Startup.Mode",
-          "name": "mode",
-          "type": "uint8"
-        },
-        {
-          "internalType": "string",
-          "name": "logo",
-          "type": "string"
-        },
-        {
-          "internalType": "string",
-          "name": "mission",
-          "type": "string"
-        },
-        {
-          "internalType": "address",
-          "name": "tokenContract",
-          "type": "address"
-        },
-        {
-          "internalType": "string",
-          "name": "overview",
-          "type": "string"
-        },
-        {
-          "internalType": "bool",
-          "name": "isValidate",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address payable",
-          "name": "receiver",
-          "type": "address"
-        }
-      ],
-      "name": "suicide0",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "newOwner",
-          "type": "address"
-        }
-      ],
-      "name": "transferOwnership",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "stateMutability": "payable",
-      "type": "receive"
-    }
-  	]`,
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"internalType": "enum Startup.Mode",
+						"name": "mode",
+						"type": "uint8"
+					},
+					{
+						"internalType": "string",
+						"name": "logo",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "mission",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "overview",
+						"type": "string"
+					},
+					{
+						"internalType": "bool",
+						"name": "isValidate",
+						"type": "bool"
+					}
+				],
+				"indexed": false,
+				"internalType": "struct Startup.Profile",
+				"name": "startUp",
+				"type": "tuple"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "msg",
+				"type": "address"
+			}
+		],
+		"name": "created",
+		"type": "event"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "fallback"
+	},
+	{
+		"inputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "name",
+						"type": "string"
+					},
+					{
+						"internalType": "enum Startup.Mode",
+						"name": "mode",
+						"type": "uint8"
+					},
+					{
+						"internalType": "string",
+						"name": "logo",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "mission",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "overview",
+						"type": "string"
+					},
+					{
+						"internalType": "bool",
+						"name": "isValidate",
+						"type": "bool"
+					}
+				],
+				"internalType": "struct Startup.Profile",
+				"name": "p",
+				"type": "tuple"
+			}
+		],
+		"name": "newStartup",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "startups",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "enum Startup.Mode",
+				"name": "mode",
+				"type": "uint8"
+			},
+			{
+				"internalType": "string",
+				"name": "logo",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "mission",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "overview",
+				"type": "string"
+			},
+			{
+				"internalType": "bool",
+				"name": "isValidate",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "receiver",
+				"type": "address"
+			}
+		],
+		"name": "suicide0",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	}
+]`,
 	}
 )
--- pkg/event/handle.go
+++ pkg/event/handle.go
@@ -4,10 +4,7 @@ import (
 	"ceres/pkg/initialization/mysql"
 	"ceres/pkg/model/account"
 	model "ceres/pkg/model/startup"
-	"ceres/pkg/model/tag"
 	"errors"
-
-	"github.com/ethereum/go-ethereum/common"
 
 	"gorm.io/gorm"
 
@@ -16,16 +13,16 @@ import (
 
 func HandleStartup(address string, startupProto interface{}) {
 	startupTemp := startupProto.(struct {
-		Name          string         `json:"name"`
-		Mode          uint8          `json:"mode"`
-		Hashtag       []string       `json:"hashtag"`
-		Logo          string         `json:"logo"`
-		Mission       string         `json:"mission"`
-		TokenContract common.Address `json:"tokenContract"`
-		Wallets       []struct {
-			Name          string         `json:"name"`
-			WalletAddress common.Address `json:"walletAddress"`
-		} `json:"wallets"`
+		Name string `json:"name"`
+		Mode uint8  `json:"mode"`
+		//Hashtag       []string       `json:"hashtag"`
+		Logo    string `json:"logo"`
+		Mission string `json:"mission"`
+		//TokenContract common.Address `json:"tokenContract"`
+		//Wallets       []struct {
+		//	Name          string         `json:"name"`
+		//	WalletAddress common.Address `json:"walletAddress"`
+		//} `json:"wallets"`
 		Overview   string `json:"overview"`
 		IsValidate bool   `json:"isValidate"`
 	})
@@ -40,61 +37,61 @@ func HandleStartup(address string, startupProto interface{}) {
 		return
 	}
 	startup := model.Startup{
-		ComerID:              comer.ID,
-		Name:                 startupTemp.Name,
-		Mode:                 model.Mode(startupTemp.Mode),
-		Logo:                 startupTemp.Logo,
-		Mission:              startupTemp.Mission,
-		TokenContractAddress: startupTemp.TokenContract.String(),
-		Overview:             startupTemp.Overview,
+		ComerID: comer.ID,
+		Name:    startupTemp.Name,
+		Mode:    model.Mode(startupTemp.Mode),
+		Logo:    startupTemp.Logo,
+		Mission: startupTemp.Mission,
+		//TokenContractAddress: startupTemp.TokenContract.String(),
+		Overview: startupTemp.Overview,
 	}
 	if err := mysql.DB.Transaction(func(tx *gorm.DB) (er error) {
 		//create startup
 		if er = model.CreateStartup(tx, &startup); er != nil {
 			return
 		}
-		var wallets []model.Wallet
-		for _, v := range startupTemp.Wallets {
-			wallets = append(wallets, model.Wallet{
-				ComerID:       comer.ID,
-				StartupID:     startup.ID,
-				WalletName:    v.Name,
-				WalletAddress: v.WalletAddress.String(),
-			})
-		}
-		//create startup wallet
-		if er = model.CreateStartupWallet(tx, wallets); er != nil {
-			return
-		}
-		//create skill
-		var tagRelList []tag.TagTargetRel
-		for _, skillName := range startupTemp.Hashtag {
-			var isIndex bool
-			if len(skillName) > 2 && skillName[0:1] == "#" {
-				isIndex = true
-			}
-			skill := tag.Tag{
-				Name:     skillName,
-				IsIndex:  isIndex,
-				Category: tag.Startup,
-			}
-			if er = tag.FirstOrCreateTag(tx, &skill); er != nil {
-				return er
-			}
-			tagRelList = append(tagRelList, tag.TagTargetRel{
-				TagID:    skill.ID,
-				Target:   tag.Startup,
-				TargetID: startup.ID,
-			})
-		}
-		//batch create comer skill relation
-		if len(tagRelList) == 0 {
-			return er
-		}
-		if er = tag.BatchCreateTagRel(tx, tagRelList); er != nil {
-			log.Warn(er)
-			return er
-		}
+		//var wallets []model.Wallet
+		//for _, v := range startupTemp.Wallets {
+		//	wallets = append(wallets, model.Wallet{
+		//		ComerID:       comer.ID,
+		//		StartupID:     startup.ID,
+		//		WalletName:    v.Name,
+		//		WalletAddress: v.WalletAddress.String(),
+		//	})
+		//}
+		////create startup wallet
+		//if er = model.CreateStartupWallet(tx, wallets); er != nil {
+		//	return
+		//}
+		////create skill
+		//var tagRelList []tag.TagTargetRel
+		//for _, skillName := range startupTemp.Hashtag {
+		//	var isIndex bool
+		//	if len(skillName) > 2 && skillName[0:1] == "#" {
+		//		isIndex = true
+		//	}
+		//	skill := tag.Tag{
+		//		Name:     skillName,
+		//		IsIndex:  isIndex,
+		//		Category: tag.Startup,
+		//	}
+		//	if er = tag.FirstOrCreateTag(tx, &skill); er != nil {
+		//		return er
+		//	}
+		//	tagRelList = append(tagRelList, tag.TagTargetRel{
+		//		TagID:    skill.ID,
+		//		Target:   tag.Startup,
+		//		TargetID: startup.ID,
+		//	})
+		//}
+		////batch create comer skill relation
+		//if len(tagRelList) == 0 {
+		//	return er
+		//}
+		//if er = tag.BatchCreateTagRel(tx, tagRelList); er != nil {
+		//	log.Warn(er)
+		//	return er
+		//}
 		return er
 	}); err != nil {
 		log.Warn(err)
